d96b16e0f2ead2644e9d2d5a7ea8c607f3dbf522
nishy <nishyjana6435@gmail.com>
2020-10-31 02:36:58 +0530

    details front

--- src/App.js
+++ src/App.js
@@ -1,20 +1,36 @@
 import React from 'react';
+import Product from './components/products';
+import data from './data.json';
+class App extends React.Component{
+  constructor(){
+    super();
+    this.state={
+      products:data.products,
+      size:"",
+      sort:""
+    }
+  }
+  render(){
+    return (
+      <div className="grid-container">
+        <header>
+          <a href="/">NISHY SHOPPING</a> 
+        </header>
+        <main>
+          <div className="content">
+            <div className="main"><Product   products={this.state.products}></Product></div>
+            <div className="sidebar">Card Items </div>
 
-function App() {
-  return (
-    <div className="grid-container">
-      <header className="">
-        <a href="/">NISHY SHOPPING</a> 
-      </header>
-      <main>
-        PRDUCT LIST
-      </main>
-      <footer>
-        All Rights Reserverd <br></br>
-       
-      </footer>
-    </div>
-  );
+          </div>
+    
+        </main>
+        <footer>
+          All Rights Reserverd <br></br>
+        </footer>
+      </div>
+    );
+  }
+
 }
 
 export default App;
